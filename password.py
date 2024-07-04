ef7b948373cfa6d58294e03aef5c2fd8f3932192
Oleh Malytskyi <olegmal574@gmail.com>
2024-07-04 22:06:54 +0100

    Finished GUI.

--- password.py
+++ password.py
@@ -38,34 +38,29 @@ class ImplGUI:
     def __init__(self, passfile):
         self.passfile = passfile
         self.root = Tk()
-        self.root.geometry("1640x1900")
+        self.root.geometry("1600x1900")
         self.root.title("Password")
+        self.root.configure(bg="gray23")
         self.set_gui()
 
     def set_gui(self):
         font_settings = ("Helvetica", 14)
 
-        self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
-        self.root.rowconfigure(1, weight=1)
-        self.root.rowconfigure(2, weight=1)
+        self.l = Label(self.root, text="What is the password?", font=font_settings, fg="white", bg="gray23")
+        self.l.place(relx=0.5, rely=0.45, anchor=CENTER)
 
-        self.l = Label(text="What is the password?")
-        self.l.grid(row=0, column=0, pady=20)
+        self.inputtxt = Text(self.root, height=1, width=25, bg="light yellow", font=font_settings, fg="black")
+        self.inputtxt.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-        self.inputtxt = Text(self.root, height=10,width=25,bg="light yellow", font=font_settings)
-        self.inputtxt.grid(row=1, column=0, pady=20)
+        self.display = Button(self.root, height=2, width=14, text="Try", font=font_settings,
+                              command=lambda: self.Take_input(), bg="white")
+        self.display.place(relx=0.5, rely=0.62, anchor=S)
 
-        self.display = Button(self.root, height=2,width=20,text="Continue",command=lambda: self.Take_input())
-        self.display.grid(row=2, column=0, pady=20)
 
-        """self.l.pack()
-        self.inputtxt.pack()
-        self.display.pack()"""
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.bind('<Unmap>', self.on_minimize)
         self.lock_position()
-        # self.root.attributes('-topmost', True)
+        self.root.attributes('-topmost', True)
         self.root.resizable(False, False)
         self.root.mainloop()
 
